720748a7daeca4416a81f7d91ed37e5e3dfd74aa
mercenarioz <funnystar320@gmail.com>
2024-01-22 01:23:08 +0700

    layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,10 +9,23 @@ export default function Home() {
           <h1 className="font-semibold text-xl">Welcome</h1>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-2">
-            <ListItem />
+            <ListItem
+              image="/images/liked.png"
+              name="Liked songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+
+      <div className="mt-2 mb-6 px-6">
+        <div>
+          <h1 className="text-xl font-semibold">Newest songs</h1>
+        </div>
+
+        {/* List of songs */}
+        <div>List of songs</div>
+      </div>
     </div>
   );
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   name: string;
@@ -18,9 +19,23 @@ const ListItem: React.FC<ListItemProps> = ({ name, href, image }) => {
   };
 
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/25 transition px-2">
+    <button
+      onClick={onClick}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/25 transition"
+    >
       <div className="relative min-w-[60px] min-h-[60px]">
-        <Image />
+        <Image
+          alt="liked"
+          src={image}
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <p className="font-medium text-sm truncate py-4">{name}</p>
+
+      <div className="absolute flex items-center justify-center right-4 rounded-full bg-green-600 p-3 drop-shadow-md group-hover:opacity-100 opacity-0 hover:scale-110 transition">
+        <FaPlay />
       </div>
     </button>
   );
